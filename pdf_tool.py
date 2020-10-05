68d2e32fcb36e8d55413065a18436460dbf0b5f1
Dhronaachrea <72329268+Dhronaachrea@users.noreply.github.com>
2020-10-05 20:40:32 +0530

    Update pdf_tool.py

--- pdf_tool.py
+++ pdf_tool.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter.ttk import *
 from tkinter import ttk
+from tkinter import filedialog as fd
+from functions import docx_files, pic_files, text_files
 
 class Pdf_Tool(object):
     def __init__(self,master):
@@ -15,7 +17,7 @@ class Pdf_Tool(object):
         self.file_label.grid(column=0,row=0,padx=5)
         self.file_entry=Entry(self.selection_frame,width=25)
         self.file_entry.grid(column=1,row=0,padx=5)
-        self.set_button=Button(self.selection_frame,text='Set')
+        self.set_button=Button(self.selection_frame,text='Set',command=self.file_field)
         self.set_button.grid(column=2,row=0,padx=5)
         
         self.file_label=Label(self.selection_frame,text='Format: ')
@@ -23,11 +25,27 @@ class Pdf_Tool(object):
         self.from_menu=ttk.Combobox(self.selection_frame,values=self.options)
         self.from_menu.grid(column=1,row=1,padx=5)
         self.from_menu.current(0)
-        self.convert_button=Button(self.selection_frame,text='convert')
+        self.convert_button=Button(self.selection_frame,text='convert',command=self.file_extension)
         self.convert_button.grid(column=2,row=1)
         
         self.result_label=Label(self.selection_frame,text='')
         self.result_label.grid(column=0,row=2,columnspan=3)
+    
+    def file_field(self):
+        self.file_path=fd.askopenfilename()
+        self.file_entry.delete(0,END)
+        self.file_entry.insert(0,str(self.file_path))
+        
+    def file_extension(self):
+        file_ext=self.file_path.split('\\')[-1]
+        file_ext=file.split('.')[-1]
+        file_formats={'pdf':text_files,"png":pic_files,'txt':text_files,'docx':docx_files}
+        
+        convert_to=self.from_menu.get()
+        convertion=file_formats[convert_to]
+        convertion=convertion(self.file_path)
+        convertion()
+        self.result_label.configure(text='File converted')
         
 if __name__ == '__main__':
     root=Tk()
